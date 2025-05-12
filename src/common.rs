5f41ec737ea92028ef31c1442caed82a3a0d68ab
Shiroko3 <mhy1630005@163.com>
2025-05-12 19:47:04 +0800

    update

--- src/common.rs
+++ src/common.rs
@@ -990,7 +990,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://api-sp-2.ru2023.top:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {

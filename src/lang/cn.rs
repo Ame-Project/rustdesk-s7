8251af8372d58d0b06f3add5a4b84adb8eb1fdf2
Shiroko3 <mhy1630005@163.com>
2025-06-08 10:18:36 +0800

    同步官方提交至 rustdesk/rustdesk@83f45b2

--- src/lang/cn.rs
+++ src/lang/cn.rs
@@ -692,11 +692,12 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("{} Update", "{} 更新"),
         ("{}-to-update-tip", "即将关闭 {} ，并安装新版本。"),
         ("download-new-version-failed-tip", "下载失败，您可以重试或者点击\"下载\"按钮，从发布网址下载，并手动升级。"),
-        ("Auto update", "自动更新"),
+        ("Auto update", "自动更新(由于是非官方客户端,不建议开启)"),
         ("update-failed-check-msi-tip", "安装方式检测失败。请点击\"下载\"按钮，从发布网址下载，并手动升级。"),
         ("websocket_tip", "使用 WebSocket 时，仅支持中继连接。"),
         ("Use WebSocket", "使用 WebSocket"),
         ("Trackpad speed", "触控板速度"),
         ("Default trackpad speed", "默认触控板速度"),
+        ("Numeric one-time password", "一次性密码为数字"),
     ].iter().cloned().collect();
 }
